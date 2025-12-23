50b546a4860d667005ded0a4058c0a114c49094d
Shoaib Ahmed Siddiqui <m.shoaib_ahmed@hotmail.com>
2025-12-23 01:03:34 +0000

    Eval updated

--- train.sh
+++ train.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
 epochs=1
-python train.py --model_name "next-tok-ep"${epochs} --max_epochs ${epochs} --use_wandb True
-python train.py --model_name "next-lat-pad-fixed-ep"${epochs} --max_epochs ${epochs} --next_lat_pred True --use_wandb True
+data="shortest-paths"
+
+base_args="--data ${data}"
+next_tok_args="${base_args} --model_name ${data}-next-tok-ep${epochs}"
+next_lat_args="${base_args} --model_name ${data}-next-lat-ep${epochs}"
+
+# Train models
+python train.py ${next_tok_args} --max_epochs ${epochs} --use_wandb True
+python train.py ${next_lat_args} --next_lat_pred True --max_epochs ${epochs} --use_wandb True
 
 # Perform detour analysis
-python detour_analysis.py --model-name "next-tok-ep"${epochs}
-python detour_analysis.py --model-name "next-lat-pad-fixed-ep"${epochs} --next-lat-pred
+python detour_analysis.py ${next_tok_args}
+python detour_analysis.py ${next_lat_args} --next-lat-pred
+
+# Perform compression test
+python compression_test.py ${next_tok_args}
+python compression_test.py ${next_lat_args} --next-lat-pred
